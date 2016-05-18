daf21419968032e172ab887bf2c36e87565b63a4
OptimAdam <OptimAdam@hotmail.com>
2016-05-18 16:14:55 -0700

    Load User Details
    Loads user details

---
+++ ADFileLoad.ps1
@@ -0,0 +1,32 @@
+﻿#----------------------------------------------------------
+# LOAD ASSEMBLIES AND MODULES
+#----------------------------------------------------------
+Try
+{
+  Import-Module ActiveDirectory -ErrorAction Stop
+}
+Catch
+{
+  Write-Host "[ERROR]`t ActiveDirectory Module couldn't be loaded. Script will stop!"
+  Exit 1
+}
+
+#----------------------------------------------------------
+#STATIC VARIABLES
+#----------------------------------------------------------
+$users = Import-CSV -Delimiter "," .\USers.csv
+
+
+#----------------------------------------------------------
+#User creation
+#----------------------------------------------------------
+foreach ($User in $Users)            
+{
+   $Displayname = $User.Firstname + " " + $User.Lastname
+   $UserFirstname = $User.Firstname
+   $UserLastname = $User.Lastname
+   $Email = $user.email
+   $Company = $user.Company
+   write-host $Company
+
+}
