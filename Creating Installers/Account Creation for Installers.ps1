242802192f0816e3cd6fe7e527b64d3e11315fea
OptimAdam <OptimAdam@hotmail.com>
2016-12-08 11:19:02 -0800

    Created file

---
+++ Creating Installers/Account Creation for Installers.ps1
@@ -0,0 +1,61 @@
+﻿#----------------------------------------------------------
+# LOAD ASSEMBLIES AND MODULES
+#----------------------------------------------------------
+Try
+{
+  Import-Module ActiveDirectory -ErrorAction Stop
+}
+Catch
+{
+  Write-Host "[ERROR]`t ActiveDirectory Module couldn't be loaded. Script will stop!"
+  Exit 1
+}
+
+#----------------------------------------------------------
+#STATIC VARIABLES
+#----------------------------------------------------------
+$users = Import-CSV -Delimiter "," .\Users.csv
+$Suffix = "@PeakInstallations.com"
+$OU = "OU=Inspectors,OU=Contractors,OU=External Users,OU=Peak,DC=peak,DC=internal"
+
+#----------------------------------------------------------
+#User creation
+#----------------------------------------------------------
+foreach ($User in $Users)            
+{
+   $Displayname = $User.Firstname + " " + $User.Lastname
+   $Username = $User.Firstname + "." + $User.Lastname
+   $UserPrincipalName = $Username + "@PeakInstallations.com"
+   $UserFirstname = $User.Firstname
+   $UserLastname = $User.Lastname
+   $Email = $user.email
+   $Phone = $user.phone
+   $Company = $user.Company
+   $Addresses = $user.Address
+   $City = $user.city
+   $Post_Code = $user.post_code
+#create random number for password
+   $random = get-random -Maximum 9999
+   $Password = $userfirstname.Substring(0,2)+ $UserLastname.Substring(0,2)+ "_" + $random 
+   $Securepassword = convertto-securestring $Password -asplaintext -force
+   Write-Host $UserPrincipalName ", " $Password
+
+#create user set SMTP proxy address, ou path
+    new-aduser $Displayname -SamAccountName $Username -UserPrincipalName $UserPrincipalName -EmailAddress $UserPrincipalName -Company $Company -AccountPassword $Securepassword -PasswordNeverExpires $true -GivenName $UserFirstname -DisplayName $Displayname -Surname $UserLastname -Office EXCLUDE -OtherAttributes @{'proxyAddresses'="SMTP:$Email"} -Path "OU=Inspectors,OU=Contractors,OU=External Users,OU=Peak,DC=peak,DC=internal"
+
+#add to groups
+$Groups = "domain guests","Installations - Installers"
+ForEach ($Group in $Groups)
+    {Add-ADGroupMember $Group  $Username}
+
+#set primary group
+$group = get-adgroup "domain guests" -properties @("primaryGroupToken")
+get-aduser $Username | set-aduser -replace @{primaryGroupID=$group.primaryGroupToken}
+
+#remove group
+remove-adgroupmember -Identity "Domain Users" -Member $Username -Confirm:$false
+
+#enable account.
+Enable-ADAccount -Identity $Username 
+
+}
